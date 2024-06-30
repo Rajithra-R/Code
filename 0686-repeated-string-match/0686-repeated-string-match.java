a9c57fe1132f879fe1044ba266580e70fc03b817
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-06-30 14:17:04 +0530

    Time: 271 ms (20.61%), Space: 45 MB (22.36%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -3,7 +3,7 @@ class Solution {
         int n  = b.length() / a.length();
         String s = a;  // substring that hold the initial 1st string
         int count = 1; // include the 1st string initially  
-        for(int i =0;i<=n+1;i++)
+        for(int i =0;i<n+2;i++)
         {
             if(s.contains(b))   
       // at some cond it will definitely fall in this base case as we compare using s a new var
