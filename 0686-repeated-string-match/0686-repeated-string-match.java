fc719b855132165d5da6104a7451a5721686164f
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-06-30 14:15:19 +0530

    Time: 271 ms (20.61%), Space: 45 MB (16.50%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+        int n  = b.length() / a.length();
+        String s = a;  // substring that hold the initial 1st string
+        int count = 1; // include the 1st string initially  
+        for(int i =0;i<=n+1;i++)
+        {
+            if(s.contains(b))   
+      // at some cond it will definitely fall in this base case as we compare using s a new var
+            {
+                return count;
+            }
+            else
+            {
+                s = s+a; // add 1st string second time to a
+                count++;  // further addition is done  in s and the counter is incremented
+            }
+        }
+        return -1;
+    }
+}
