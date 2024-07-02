b5c3a37adceb9e7a3606f545f17d83c1f7ff7d6d
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-07-02 20:48:16 +0530

    Time: 25 ms (7.31%), Space: 45.3 MB (5.31%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,25 @@
+//O(n * m)
+class Solution {
+    public String countAndSay(int n) {
+        String cur = "1";
+        for(int j=1;j<n;j++)
+        {
+            String res = "";
+            int i = 0;
+            while(i< cur.length())
+            {
+                int count = 1;
+                 while(i < cur.length()-1 && cur.charAt(i) == cur.charAt(i+1))  
+                // need not check for last ele as last ele dont have i+1
+                 {
+                        count ++;
+                        i++;
+                 }
+                res += Integer.toString(count) + cur.charAt(i);
+                i++;
+            }
+            cur = res;
+        }
+        return cur;
+    }
+}
