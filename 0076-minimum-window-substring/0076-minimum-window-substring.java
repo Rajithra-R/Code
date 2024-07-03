bf2baae25d562338ec9f303d0e20895eab626432
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-07-03 11:20:25 +0530

    Time: 4 ms (74.58%), Space: 44.4 MB (89.33%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -12,22 +12,22 @@ class Solution {
         int count = 0;
 
         while (right < s.length()) {
-            char c1 = s.charAt(right);
-            window[c1]++;
-            if (t_arr[c1] > 0 && window[c1] <= t_arr[c1]) {
-                count++;
+            char c1 = s.charAt(right);  
+            window[c1]++;  // increment the frq and check for validity
+            if (t_arr[c1] > 0 && window[c1] <= t_arr[c1]) { //move upto a string become valid
+                count++;    //  count is use to keep up the validity
             }
 
-            while (count == t.length()) {
-                if (right - left + 1 < minLen) {
+            while (count == t.length()) {   // that means that all char in t is encountered so the substr now we have is valid
+                if (right - left + 1 < minLen) {    // update min and reduce count
                     minLen = right - left + 1;
                     start = left;
                 }
 
                 char c2 = s.charAt(left);
-                window[c2]--;
-                if (t_arr[c2] > 0 && window[c2] < t_arr[c2]) {
-                    count--;
+                window[c2]--;   // decrement count
+                if (t_arr[c2] > 0 && window[c2] < t_arr[c2]) {  // traverse until it become invalid
+                    count--; //and decrement count i.e if there are 2 char in t and 1 is decremented then the substr become invalid 
                 }
                 left++;
             }
