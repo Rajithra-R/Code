9e8bba2e02aca537e17e908d2021d2989c9cf9ef
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-07-05 16:24:49 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (68.03%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+// n = 3 => 1+1+1 ; 1+2 ; 2+1 =>3
+// n = 5 => 1+1+1+1+1 ; 1+1+1+2 ; 1+1+2+1 ; 1+2+1+1 ; 2+1+1+1 ; 2+2+1 ; 1+2+2 ; 2+1+2 => 8
+// This is an anothere version of FIBONACCI SERIES
+// Upto n=3  => no of steps is 3 itself then for n=4 -> 5; n=5 ->8; n=6 -> 13.......
+
+class Solution {
+    public int climbStairs(int n) { 
+       int a = 0;
+       int b = 1;
+        int sum = 0;
+       for(int i=0;i<n;i++)
+       {
+           sum = a+b;
+           a = b;
+           b = sum;
+       }
+        return sum;
+    }
+}
