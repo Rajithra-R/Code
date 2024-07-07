4f11dec4837f24d2b60d07d5879f401ba5d14439
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-07-07 19:27:47 +0530

    Time: 2 ms (82.68%), Space: 46.4 MB (7.87%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        //initially nerver mind the zeros
+        // if a non zero ele comes then put it in count and increment count
+        //after all non zero ele is traversed 
+        //put zero for all the remaining elements
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] != 0)
+            {
+                nums[count] = nums[i];
+                count++;
+            } 
+        }
+        while(count < n) // all the remaining elements
+        {
+            nums[count++] = 0;
+        }
+    }
+}
