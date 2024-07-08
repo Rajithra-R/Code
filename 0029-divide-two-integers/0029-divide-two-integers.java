9a0f9b2601890333cbba3a79d2a4801500a458e7
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-07-08 23:40:56 +0530

    Time: 1 ms (72.22%), Space: 41.4 MB (6.47%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -1,3 +1,8 @@
+
+// 10 / 3 => 
+//  10   10   10
+//  3    6    12   10<12 stop -> count = 2  // the divisor is incremented 1<<count times using << operator
+
 class Solution {
     public int divide(int dividend, int divisor) {
         // initially handle the overflow case
@@ -7,10 +12,10 @@ class Solution {
         dividend = Math.abs(dividend);
         divisor = Math.abs(divisor);
         int res = 0;
-        while(dividend - divisor >= 0)
+        while(dividend - divisor >= 0)   // do until the subtraction is >0
         {
             int count = 0;
-            while(dividend - (divisor << 1 << count) >= 0)
+            while(dividend - (divisor << 1 << count) >= 0)  
             {
                 count++;
             }
