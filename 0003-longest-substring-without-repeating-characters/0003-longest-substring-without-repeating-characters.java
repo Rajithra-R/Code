dc974132a8c9a5f2fa31b55f265cef184ff045ed
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-07-20 13:30:54 +0530

    Time: 8 ms (26.27%), Space: 44.7 MB (37.20%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,18 +4,18 @@ class Solution {
         int max = 0;
         int low = 0;
         int high = 0;
-        while(low < s.length())
+        while(high < s.length())
         {
-            if(!h.contains(s.charAt(low))) // if the char not in the hash
+            if(!h.contains(s.charAt(high))) // if the char not in the hash
             {
-                h.add(s.charAt(low));   // add the char to the hash
-                low++;                  // increment the pointer
-                max = Math.max(max,low-high);   // max is maximum of max and the size of the substring
+                h.add(s.charAt(high));   // add the char to the hash
+                high++;                  // increment the pointer
+                max = Math.max(max,high-low);   // max is maximum of max and the size of the substring
             }
             else  // already present...pop out and move high
             {
-                h.remove(s.charAt(high));
-                high++;    
+                h.remove(s.charAt(low));
+                low++;    
             }
             // its of if u reverse the pointers too hight as low or low as high
         }
