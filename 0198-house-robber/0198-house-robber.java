daa4cbbf59d3f095ce690ce6ab7144940911e552
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-07-25 15:59:27 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (81.30%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0) return 0;
-        if(nums.length == 1) return nums[0];
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
-        for(int i=2;i<nums.length;i++)
-        {
-            dp[i] = Math.max(dp[i-1],nums[i]+dp[i-2]);
-        }
-        return dp[nums.length-1];
+       int n =nums.length;
+       int prev1 = nums[0];
+       int prev2 = 0;
+       int cur = 0;
+       for(int i=1;i<n;i++)
+       {
+           int pick = nums[i];
+           if(i-2 >= 0)
+           {
+               pick += prev2;
+           }
+           int notpick = 0 + prev1;
+           cur = Math.max(pick,notpick);
+           prev2 = prev1;
+           prev1 = cur;
+       }
+        return prev1;
     }
 }
 
-// update each element by taking the max with the prev element in dp and sum of current element in nums and 2d prev element in dp
