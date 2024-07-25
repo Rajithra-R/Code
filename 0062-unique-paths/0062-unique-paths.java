7ec2ea316cc332a7b2653fb37ed223045d5d91fb
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-07-25 17:28:44 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (51.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,24 @@
+// tabulation
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++)  //traverse each row of 1st col
+       
+        int dp[][] = new int[m][n];
+        for(int i=0;i<m;i++)
         {
-            dp[i][0] = 1;
-        }
-        for(int j=0;j<n;j++)  //traverse each col of 1st row
-        {
-            dp[0][j] = 1;
-        }
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
+            for(int j=0;j<n;j++)
             {
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                if(i==0 && j==0) dp[i][j] = 1;
+                else
+                {
+                    int up = 0,left = 0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                    
+                }
             }
         }
-        //return the last element
+             //return the last element
         return dp[m-1][n-1];
     }
 }
