ae65361f63e951efca393faeb62f1a0c3e81db20
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-07-25 17:54:08 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (41.47%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,34 +2,30 @@
 
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-         int m = obstacleGrid.length;
+        int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-       // int dp[][] = new int[m][n];
-       int prev[] = new int[n];
+        int dp[][] = new int[m][n];
         for(int i=0;i<m;i++)
         {
-        int cur[] = new int[n];
             for(int j=0;j<n;j++)
             {
                 if(obstacleGrid[i][j] == 1)  // if u fing an obstacle just continue
                 {
-                  //  cur[j] = 0;
                     continue;
                 }
-                if(i==0 && j==0) cur[j] = 1;
+                if(i==0 && j==0) dp[i][j] = 1;
                 else
                 {
                     int up = 0,left = 0;
-                    if(i>0) up = prev[j];
-                    if(j>0) left = cur[j-1];
-                    cur[j] = up + left;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
                     
                 }
             }
-            prev = cur;
         }
              //return the last element
-        return prev[n-1];
+        return dp[m-1][n-1];
     }
 }
 
@@ -58,6 +54,7 @@ space optimization => cur = dp[i] ; prev = d[i-1]
                     
                 }
             }
+            prev = cur;
         }
              //return the last element
         return prev[n-1];
