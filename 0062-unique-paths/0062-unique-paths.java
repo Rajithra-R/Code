7ad5b8c3f2a624bd3995bb3108ebe033ff41bdaf
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-07-25 17:36:08 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (37.98%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,54 @@
 // tabulation
 class Solution {
     public int uniquePaths(int m, int n) {
-       
-        int dp[][] = new int[m][n];
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i==0 && j==0) dp[i][j] = 1;
+        
+       int prev[] = new int[n];
+//prev = {0};
+for(int i=0;i<m;i++)
+{
+   int cur[] = new int[n];
+//   cur = {0};
+   for(int j=0;j<n;j++)
+     {
+     if(i==0 && j==0) cur[j] = 1;
                 else
                 {
                     int up = 0,left = 0;
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
-                    dp[i][j] = up + left;
-                    
+                    if(i>0) up = prev[j];
+                    if(j>0) left = cur[j-1];
+                    cur[j] = up + left;
                 }
-            }
         }
-             //return the last element
-        return dp[m-1][n-1];
+        prev = cur;
+}
+return prev[n-1];
+       
     }
 }
+
+
+// space optimizition
+/*
+use variables => prev,cur   => cur = dp[i] & prev = dp[i-1]
+int prev[] = new int[n];
+prev = {0};
+for(int i=0;i<m;i++)
+{
+   int cur[] = new int[n];
+   cur = {0};
+   for(int j=0;j<n;j++)
+     {
+     if(i==0 && j==0) cur[j] = 1;
+                else
+                {
+                    int up = 0,left = 0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = cur[j-1];
+                    cur[j] = up + left;
+                }
+        }
+        prev = cur;
+}
+return prev[n-1];
+       
+   */ 
