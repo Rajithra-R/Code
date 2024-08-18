aa2f22eb7134cff2cbf9c619556083d2e51ee5e9
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-08-18 10:33:48 +0530

    Time: 1 ms (84.41%), Space: 41.8 MB (32.24%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,24 @@
+// cooldown - buy and sell then the next sudden day u cannot buy another .... 
+// cannot buy the next day after sell and hence sell condition can be i+2
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int dp[][] = new int[n+2][2]; // n+2 - since dealing with index +2
+        int i = 0;
+        if(i == n) return 0;
+      //  if(dp[i][buy] != -1) return dp[i][buy];
+         for(i = n - 1; i >= 0; i--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                if(buy == 1)
+                {
+                    dp[i][buy] = Math.max(-prices[i] + dp[i+1][0] , 0 + dp[i+1][1]);
+                }
+                else // sell
+                {
+                    dp[i][buy] = Math.max(prices[i] + dp[i+2][1] , 0 + dp[i+1][0] );// if i is 1st day then i+2 to buy the third day
+                }
+            }
+         }
+        return dp[0][1];
+    }
+}
