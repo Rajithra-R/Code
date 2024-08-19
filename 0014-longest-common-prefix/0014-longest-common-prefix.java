ed8e4211ee9558e0fb8a1cf4f17649533d40e591
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-08-19 20:24:19 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (33.95%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,18 +4,19 @@ class Solution {
         String s1 = strs[0];
         String s2 = strs[strs.length-1];
         int i = 0;
+        int max = 0;
         while(i < s1.length())
         {
             if(s1.charAt(i) == s2.charAt(i))
             {
-                i++;
+               max = i;
             }
             else
             {
                 break;
             }
-            
+             i++;
         }
-        return i == 0 ? "" : s1.substring(0,i);
+        return i == 0 ? "" : s1.substring(0,max+1);
     }
 }
