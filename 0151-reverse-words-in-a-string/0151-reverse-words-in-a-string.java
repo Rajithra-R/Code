2babef1699d92d5f16913459e1b83b41818e9047
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-08-19 20:18:53 +0530

    Time: 6 ms (85.50%), Space: 42.8 MB (83.04%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,6 +1,7 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.split(" +");
+        String[] words = s.split(" +");   
+        // the reason why dont we use trim here is that there may be more than 1 spaces in the middle of string resulting in error
         StringBuilder b = new StringBuilder();
         for(int i = words.length-1;i>=0;i--)
         {
@@ -10,3 +11,7 @@ class Solution {
         return b.toString().trim();
     }
 }
+
+
+//s.split(" +")  - For example, s = "a b c" would result in ["a", "b", "c"]
+// s.split(" ")  - For example, s = "a b c" would result in ["a", "", "b", "", "", "c"]
