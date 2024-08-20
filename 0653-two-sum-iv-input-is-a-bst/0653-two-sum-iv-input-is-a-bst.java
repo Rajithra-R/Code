d87fdd7ab3de5b0b1668638551f00b4944500a6f
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-08-20 20:17:36 +0530

    Time: 2 ms (95.14%), Space: 45.3 MB (30.85%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,25 +14,58 @@
  * }
  */
 class Solution {
-   static int x =0 ;// keep track of index
+   
     public boolean findTarget(TreeNode root, int k) {
+          List<Integer> nums = new ArrayList<>();
+        inorder(root, nums);
+        
+        int i = 0, j = nums.size() - 1;
+        
+        while (i < j) {
+            int sum = nums.get(i) + nums.get(j);
+            if (sum == k) {
+                return true;
+            } else if (sum < k) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        
+        return false;
+    }
+    
+    private void inorder(TreeNode root, List<Integer> nums) {
+        if (root == null) {
+            return;
+        }
+        
+        inorder(root.left, nums);
+        nums.add(root.val);
+        inorder(root.right, nums);
+    
+    }
+    
+}
+
+/*  NAIVE APPROACH
+1.FIND THE SIZE OF THE TREE USING L+R+1
+2.INITIALIZE AN ARAAY AND PUT THE INORDER IN THAT ARRAY (inoreder to keep track of index initialize the var outside the fn globally)
+3.MAINTAIN TWO POINTER I AND J ITERATE UNTIL I AND J CROSSES AND CHECK FOR THE SUM 
+
+        static int x =0 ;// keep track of index
+
          int n = size(root);
          int arr[] = new int[n];
          x = 0;
-        inorder(root,k,arr);
+         inorder(root,k,arr);
          int i=0;
         int j=arr.length-1;
         while(i < j)
         {
         if(arr[i] + arr[j] == k) return true;
-        else if(arr[i] + arr[j] < k) 
-        {
-            i++;
-        }
-        else if(arr[i] + arr[j] > k)
-        {
-            j--;
-        }
+        else if(arr[i] + arr[j] < k)  i++;
+        else if(arr[i] + arr[j] > k)  j--;
         }
         return false;
     }
@@ -55,5 +88,5 @@ class Solution {
         int r = size(root.right);
         return l+r+1;
     }
-    
-}
+
+*/
