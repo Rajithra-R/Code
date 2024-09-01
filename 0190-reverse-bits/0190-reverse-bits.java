380159a4f7d40b589e1c4db17d66b68cbd92fe62
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-09-01 11:16:16 +0530

    Time: 0 ms (100.00%), Space: 42 MB (23.82%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -2,7 +2,7 @@ public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int rev = 0;
-        for(int i=0;i<32;i++)
+       for(int i=0;i<32;i++)
         {
             rev <<= 1; // reserve a space
             rev |= (n & 1); // (n&1 - gives the last number).... append the last number to rev using  OR operator
@@ -11,3 +11,8 @@ public class Solution {
         return rev;
     }
 }
+/* 
+add a 0 at the end of rev (i.e left shift by 1)
+extract the last digit from num using ' n & 1' and add it with rev
+remove the last digit from the num (i.e right shift by 1)
+*/
