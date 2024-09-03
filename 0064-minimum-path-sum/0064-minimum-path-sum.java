40a38da3d3a1bec3f0014fbb80e7e885e579c21e
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-09-03 21:00:25 +0530

    Time: 6 ms (6.81%), Space: 47.4 MB (12.21%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -4,26 +4,23 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n= grid[0].length;
-      //  int dp[][] = new int[m][n];
-        int prev[] = new int[n];
+        int dp[][] = new int[m][n];
         for(int i=0;i<m;i++)
         {
-            int cur[] = new int[n];
             for(int j=0;j<n;j++)
             {
-                if(i == 0 && j == 0) cur[j] = grid[0][0];
+                if(i == 0 && j == 0) dp[i][j] = grid[0][0];
                 else
                 {
                     int up = Integer.MAX_VALUE;
                     int left = Integer.MAX_VALUE;
-                    if(i>0) up = grid[i][j] + prev[j];
-                    if(j>0) left = grid[i][j] + cur[j-1];
-                    cur[j] = Math.min(up,left);
+                    if(i>0) up = grid[i][j] + dp[i-1][j];
+                    if(j>0) left = grid[i][j] + dp[i][j-1];
+                    dp[i][j] = Math.min(up,left);
                 }
             }
-            prev = cur;
         }
-        return prev[n-1];
+        return dp[m-1][n-1];
     }
 }
 /*
@@ -34,7 +31,7 @@ SPACE OPTIMIZATION => cur = dp[i] ; prev = dp[i-1]
         int prev[] = new int[n];
         for(int i=0;i<m;i++)
         {
-            int cur[] = new cur[n];
+            int cur[] = new int[n];
             for(int j=0;j<n;j++)
             {
                 if(i == 0 && j == 0) cur[j] = grid[0][0];
