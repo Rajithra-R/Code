309a2c75ff99fd568c14e1b58de14473daa8ab23
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-09-04 21:43:35 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (69.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -8,12 +8,12 @@ class Solution {
        int a = 0;
        int b = 1;
         int sum = 0;
-       for(int i=1;i<=n;i++)
+       for(int i=0;i<n;i++)
        {
            sum = a+b;
            a = b;
            b = sum;
        }
-        return b;
+        return sum;
     }
 }
