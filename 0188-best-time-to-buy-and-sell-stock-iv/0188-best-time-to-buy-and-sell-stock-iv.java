3062fb23f61c2b23e33e52077d28d6ae8e9d15eb
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-09-07 14:56:55 +0530

    Time: 2 ms (83.93%), Space: 44.6 MB (40.38%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -4,7 +4,7 @@ class Solution {
         if (n == 0) return 0;
         
         // Arrays should be of size [2][3] to accommodate cap values 0, 1, and 2
-        int[][] cur = new int[2][k+1]; 
+    /*    int[][] cur = new int[2][k+1]; 
         int[][] after = new int[2][k+1];
 
         for(int i = n - 1; i >= 0; i--) {
@@ -24,6 +24,26 @@ class Solution {
                 }
             }
         }
-        return after[1][k]; 
+        return after[1][k]; */
+        int dp[][][] = new int[n+1][2][k+1];
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy=0;buy<2;buy++)
+            {
+                for(int cap=1;cap<=k;cap++)
+                {
+                    if(buy == 1) // buy
+                    {
+                        dp[i][buy][cap] = Math.max(-prices[i] + dp[i+1][0][cap] , 0 + dp[i+1][1][cap]);
+                    }
+                    else  // sell
+                    {
+                        dp[i][buy][cap] = Math.max(prices[i] + dp[i+1][1][cap-1] , 0 + dp[i+1][0][cap]);  // since cap val is decreasing start from 1
+                    }
+                }
+            }
+        }
+        return dp[0][1][k];
     }
 }
