298e298359d957e7af532c228b4fbd6b41f1a2dd
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-09-10 23:55:21 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (52.39%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -10,22 +10,22 @@ class Solution {
             return false;
         }
     }
-    public int FindRow(int[][] matrix,int target)  //iterate through the 1st col and find the row
+    public int FindRow(int[][] matrix,int target)  //iterate through the 1st col and find the row => row = mid | col = 0
     {
         int low = 0;
         int high = matrix.length - 1;
         while(low <= high)
         {
             int mid = (low+high)/2;
-            if(target >= matrix[mid][0] && target <= matrix[mid][matrix[0].length-1])
+            if(target >= matrix[mid][0] && target <= matrix[mid][matrix[0].length-1])  // 1 - last col
             {
                 return mid;
             }
-            else if(matrix[mid][0] < target)
+            else if(matrix[mid][0] < target) // col 1
             {
                 low = mid + 1;
             }
-            else if(matrix[mid][0] > target)
+            else if(matrix[mid][0] > target)  // col 1
             {
                 high = mid - 1;
             }
@@ -34,7 +34,7 @@ class Solution {
     }
     //initially we search the 1st col so col is 0 and row is mid
     //after we found col it is in the mid pos so col is mid and row is row in dex that has been found
-    public boolean binarySearch(int row,int[][] matrix,int target)
+    public boolean binarySearch(int row,int[][] matrix,int target) // => row = row | col = mid
     {
         int low = 0;
         int high = matrix[0].length - 1; //last element in that row
