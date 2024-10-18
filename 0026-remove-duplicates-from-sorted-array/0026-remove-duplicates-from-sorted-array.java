7e163aae06227825bdcd76d3ae79f67f66dce6cb
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2024-10-18 22:50:57 +0530

    Time: 1 ms (67.79%), Space: 44.6 MB (78.04%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -16,3 +16,8 @@ class Solution {
         return count;
     }
 }
+
+/*
+the reason for that the loop can be ended before last ele but it is not done because if the last element o=is not a duplicate then it should be printed , so loop is checking till the last . 
+dont check the last only when there is a duplicate
+*/
